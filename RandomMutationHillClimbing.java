a25afefec4304e171d4462b7ba0c5cb950fd8af6
iamehsanu <84843455+iamehsanu@users.noreply.github.com>
2022-07-08 18:11:38 +0500

    Add files via upload

--- RandomMutationHillClimbing.java
+++ RandomMutationHillClimbing.java
@@ -1,18 +1,52 @@
+
 package com.company;
+
+import java.util.*;
+import java.io.*;
 
 public class RandomMutationHillClimbing {
 
-    public static void main2() {
-        System.out.println("rest of the co");
+
+    //Pathname="C:\\Users\\DELL\\IdeaProjects\\untitled\\src\\com\\company\\file.txt"
+    public static List<int[]> ReadMDGfromFile(String Pathname)
+    {
+        try {
+            //Pathname: will Correspond to the input file in the running system
+            File myObj = new File(Pathname);
+            Scanner myReader = new Scanner(myObj);
+            List<int[]> mdg = new ArrayList<int[]>();
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] words = data.split(" ");
+                int[] values = Arrays.stream(words)
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                mdg.add(values);
+            }
+            myReader.close();
+            return mdg;
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+        return null;
     }
-}
-
-
-class Main {
 
     public static void main(String[] args)
     {
-        //System.out.println("rest of the code...");
-        RandomMutationHillClimbing.main2();
+        List<int[]> mdg= ReadMDGfromFile("C:\\Users\\DELL\\IdeaProjects\\untitled\\src\\com\\company\\file.txt");
+
+        if (mdg!=null) {
+            for (int[] eachRow : mdg) {
+                System.out.println(Arrays.toString(eachRow));
+            }
+        }
+
     }
+
 }
+
+
+
