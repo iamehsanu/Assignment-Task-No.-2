d2ca3a51a4374f10fca0079d3bc39d142319afb9
iamehsanu <84843455+iamehsanu@users.noreply.github.com>
2022-07-08 10:12:36 +0500

    Add files via upload

---
+++ RandomMutationHillClimbing.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class RandomMutationHillClimbing {
+
+    public static void main2() {
+        System.out.println("rest of the co");
+    }
+}
+
+
+class Main {
+
+    public static void main(String[] args)
+    {
+        //System.out.println("rest of the code...");
+        RandomMutationHillClimbing.main2();
+    }
+}
