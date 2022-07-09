a24a54931fcff11d9f51802132434b2e6d1949d0
iamehsanu <84843455+iamehsanu@users.noreply.github.com>
2022-07-09 13:35:02 +0500

    Add files via upload

--- RandomMutationHillClimbing.java
+++ RandomMutationHillClimbing.java
@@ -22,8 +22,46 @@ public class RandomMutationHillClimbing {
         this.ModuledependecyGraph=mdg;
         this.noofIterations=Iterationsnum;
     }
+    public boolean isValidMDG() {
+        int noOfNodes=ModuledependecyGraph.length;
+        //To check if the matix representation of MDG is Square or not
+        for(int i=0;i<noOfNodes;i++)
+        {
+            if(ModuledependecyGraph[i].length!=noOfNodes)
+            {
+                return false;
+            }
+        }
+        //To check if the MDG has non (1 or 0 value)
+        for(int i=0;i<noOfNodes;i++)
+        {
+            for(int j=0;j<this.ModuledependecyGraph[i].length;j++)
+            {
+                if(this.ModuledependecyGraph[i][j]!=0 && this.ModuledependecyGraph[i][j]!=1)
+                {
+                    return false;
+                }
+            }
+        }
+        //To check if the MDG is bi-directional or not
+        for(int i=0;i<noOfNodes;i++)
+        {
+            for(int j=0;j<this.ModuledependecyGraph[i].length;j++)
+            {
+                if(this.ModuledependecyGraph[i][j]!= this.ModuledependecyGraph[j][i])
+                {
+                    return false;
+                }
+            }
+        }
 
+        return true;
+    }
 
+    public void AlgorithmExecution()
+    {
+        System.out.println(isValidMDG());
+    }
     //Pathname="C:\\Users\\DELL\\IdeaProjects\\untitled\\src\\com\\company\\file.txt"
     //To read the MDG from the file
     public static int[][] ReadMDGfromFile(String Pathname)
@@ -36,9 +74,14 @@ public class RandomMutationHillClimbing {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 String[] words = data.split(" ");
+                for (int i=0;i<words.length;i++)
+                {
+                    int Value = Integer.parseInt(words[i]);
+                }
                 int[] values = Arrays.stream(words)
                         .mapToInt(Integer::parseInt)
                         .toArray();
+
                 mdg.add(values);
             }
             myReader.close();
@@ -55,6 +98,11 @@ public class RandomMutationHillClimbing {
             throw new IllegalArgumentException("File Does not exist",e);
 
         }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("MDG given in Non-Numeric Format",e);
+        }
+
+
     }
 
 
@@ -64,7 +112,7 @@ public class RandomMutationHillClimbing {
 
         int [][] mdg = ReadMDGfromFile("C:\\Users\\DELL\\IdeaProjects\\untitled\\src\\com\\company\\file.txt");
         RandomMutationHillClimbing obj =new RandomMutationHillClimbing(10,mdg);
-
+        obj.AlgorithmExecution();
 
     }
 
